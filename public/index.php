20e3224a24c8ca888d47222a12955c614e419d51
Lucas Davies <lucasdavies@gmail.com>
2016-06-26 00:11:13 +0100

    Adding bootstrap; Adding twig views

--- public/index.php
+++ public/index.php
@@ -15,6 +15,17 @@ $app['debug'] = filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN) ?: false;
 |--------------------------------------------------------------------------
 */
 
+// Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider, array(
+    'twig.path' => __DIR__ . '/../views',
+));
+
+// Add global layout for templates
+$app->before(function () use ($app) {
+    $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+});
+
+// Textlocal
 $app->register(new LucasDavies\Silex\TextlocalServiceProvider, array(
     'textlocal.username' => $_ENV['TEXTLOCAL_USERNAME'],
     'textlocal.hash'     => $_ENV['TEXTLOCAL_HASH'],
@@ -28,27 +39,39 @@ $app->register(new LucasDavies\Silex\TextlocalServiceProvider, array(
 */
 
 $app->get('/', function () use ($app) {
-    return 'Dashboard';
-});
+    return $app['twig']->render('dashboard.twig', array(
+        'title' => 'Dashboard',
+    ));
+})->bind('dashboard');
 
-$app->get('/compose', function () {
-    return 'New message';
-});
+$app->get('/compose', function () use ($app) {
+    return $app['twig']->render('compose.twig', array(
+        'title' => 'Compose',
+    ));
+})->bind('compose');
 
-$app->get('/inbox', function () {
-    return 'Inbox';
-});
+$app->get('/inbox', function () use ($app) {
+    return $app['twig']->render('inbox.twig', array(
+        'title' => 'Inbox',
+    ));
+})->bind('inbox');
 
-$app->get('/sent', function () {
-    return 'Sent';
-});
+$app->get('/sent', function () use ($app) {
+    return $app['twig']->render('sent.twig', array(
+        'title' => 'Sent',
+    ));
+})->bind('sent');
 
-$app->get('/message/{id}', function ($id) {
-    return 'Message ' . $id;
-})->assert('id', '\d+');
+$app->get('/message/{id}', function ($id) use ($app) {
+    return $app['twig']->render('message.twig', array(
+        'title' => 'Message',
+    ));
+})->assert('id', '\d+')->bind('message');
 
-$app->get('/contacts', function () {
-    return 'Contacts';
-});
+$app->get('/contacts', function () use ($app) {
+    return $app['twig']->render('contacts.twig', array(
+        'title' => 'Contacts',
+    ));
+})->bind('contacts');
 
 $app->run();
