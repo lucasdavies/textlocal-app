567a875db608e850cb78aa06c7ce813b6042142e
Lucas Davies <lucasdavies@gmail.com>
2016-06-26 11:20:52 +0100

    Adding functionality to send messages

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application;
 
 $app['debug'] = filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN) ?: false;
@@ -15,10 +17,13 @@ $app['debug'] = filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN) ?: false;
 |--------------------------------------------------------------------------
 */
 
+// Session
+$app->register(new Silex\Provider\SessionServiceProvider);
+
 // Twig templating engine
-$app->register(new Silex\Provider\TwigServiceProvider, array(
+$app->register(new Silex\Provider\TwigServiceProvider, [
     'twig.path' => __DIR__ . '/../views',
-));
+]);
 
 // Add global layout for templates
 $app->before(function () use ($app) {
@@ -26,11 +31,11 @@ $app->before(function () use ($app) {
 });
 
 // Textlocal
-$app->register(new LucasDavies\Silex\TextlocalServiceProvider, array(
+$app->register(new LucasDavies\Silex\TextlocalServiceProvider, [
     'textlocal.username' => $_ENV['TEXTLOCAL_USERNAME'],
     'textlocal.hash'     => $_ENV['TEXTLOCAL_HASH'],
     'textlocal.apiKey'   => $_ENV['TEXTLOCAL_APIKEY']
-));
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -39,39 +44,65 @@ $app->register(new LucasDavies\Silex\TextlocalServiceProvider, array(
 */
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('dashboard.twig', array(
+    return $app['twig']->render('dashboard.twig', [
         'title' => 'Dashboard',
-    ));
+    ]);
 })->bind('dashboard');
 
 $app->get('/compose', function () use ($app) {
-    return $app['twig']->render('compose.twig', array(
+    return $app['twig']->render('compose.twig', [
         'title' => 'Compose',
-    ));
+    ]);
 })->bind('compose');
 
+$app->post('/send', function (Request $request) use ($app) {
+    $recipients = $request->get('recipients');
+    $message = $request->get('message');
+    
+    // Error handling...
+    
+    // Only expect digits
+    $recipients = explode(',', $recipients);
+    $recipients = preg_replace('/\s+/', '', $recipients);
+    $recipients = array_filter($recipients, 'ctype_digit');
+    $recipients = array_unique($recipients);
+    
+    // Trim the message so we don't send any more characters that are necessary
+    $message = trim($message);
+    
+    // Send SMS
+    $app['textlocal']->sendSms(
+        $recipients,
+        $message,
+        'Textlocal Demo App'
+    );
+    
+    $app['session']->getFlashBag()->add('success', 'Message successfully sent.');
+    return $app->redirect('/');
+})->bind('send');
+
 $app->get('/inbox', function () use ($app) {
-    return $app['twig']->render('inbox.twig', array(
+    return $app['twig']->render('inbox.twig', [
         'title' => 'Inbox',
-    ));
+    ]);
 })->bind('inbox');
 
 $app->get('/sent', function () use ($app) {
-    return $app['twig']->render('sent.twig', array(
+    return $app['twig']->render('sent.twig', [
         'title' => 'Sent',
-    ));
+    ]);
 })->bind('sent');
 
 $app->get('/message/{id}', function ($id) use ($app) {
-    return $app['twig']->render('message.twig', array(
+    return $app['twig']->render('message.twig', [
         'title' => 'Message',
-    ));
+    ]);
 })->assert('id', '\d+')->bind('message');
 
 $app->get('/contacts', function () use ($app) {
-    return $app['twig']->render('contacts.twig', array(
+    return $app['twig']->render('contacts.twig', [
         'title' => 'Contacts',
-    ));
+    ]);
 })->bind('contacts');
 
 $app->run();
